9d659b2a1ea5442cad51069d1bc0cc4e2533ced8
rupali <rupalijogi2016@gmail.com>
2024-01-25 20:08:37 +0530

    my first code uploading on git

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            add();
+       }
+        public static void add()
+        {
+            int x = 45, y = 5, z;
+            z = x + y;
+            Console.WriteLine("Add is : "+ z);
+        }
+    }
+}
