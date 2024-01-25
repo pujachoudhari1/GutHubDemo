28a9fc12bb28d5dbd23fff30c61046d646874339
rupali <rupalijogi2016@gmail.com>
2024-01-25 20:41:39 +0530

    Modified code

--- Program.cs
+++ Program.cs
@@ -11,12 +11,21 @@ namespace GitHub
         static void Main(string[] args)
         {
             add();
+            sub();
        }
         public static void add()
         {
             int x = 45, y = 5, z;
             z = x + y;
             Console.WriteLine("Add is : "+ z);
+
+            Console.WriteLine(z);
+        }
+        public static void sub()
+        {
+            int x = 45, y = 5, z;
+            z = x - y;
+            Console.WriteLine("Add is : " + z);
 
             Console.WriteLine(z);
         }
